70a67905c6b95041b7747c0a52a5b8b37199ab00
aquadros1003 <jankowski.lukasz0987@gmail.com>
2023-01-09 18:51:07 +0100

    discount added and partially merged with main

---
+++ app/src/main/java/com/example/test123/BuyTicketActivity.java
@@ -0,0 +1,19 @@
+package com.example.test123;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BuyTicketActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //DELETE HEADER
+        if (getSupportActionBar() != null) getSupportActionBar().hide();
+        setContentView(R.layout.ticket_menu);
+
+        Button previous = findViewById(R.id.previous);
+        previous.setOnClickListener(view -> finish());
+
+    }
+}
